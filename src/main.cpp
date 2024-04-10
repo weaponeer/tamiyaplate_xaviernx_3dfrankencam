9bcdeda55e277b92202245308222e41c3d262981
mark hamilton <markc@someemail.com>
2024-04-10 18:14:22 -0500

    start porting python

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <QApplication>
+#include <QProcess>
+
+#include <memory>
+
+#include "roundWindow/roundWindow.hpp"
+#include "rviz_common/ros_integration/ros_node_abstraction.hpp"
+
+int main(int argc, char ** argv)
+{
+  QApplication app(argc, argv);
+  rclcpp::init(argc, argv);
+
+  auto ros_node_abs =
+    std::make_shared<rviz_common::ros_integration::RosNodeAbstraction>("rviz_render_node");
+
+  auto myviz = std::make_shared<MyViz>(&app, ros_node_abs);
+  myviz->show();
+
+  while (rclcpp::ok()) {
+    app.processEvents();
+  }
+
+  return 0;
+}
